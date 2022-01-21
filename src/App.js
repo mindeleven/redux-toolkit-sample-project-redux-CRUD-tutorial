9377832c349556b68390a6375be8111e305bc8d0
mindeleven <mindeleven@posteo.de>
2022-01-21 14:11:44 +0100

    Scaffolding input form to add user

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      {" "}
+      <div className="addUser">
+        <input type="text" placeholder="Name..." />
+        <input type="text" placeholder="Username..." />
+        <button>Add User</button>
+      </div>
+      <div className="displayUsers"></div>
+    </div>
   );
 }
 
