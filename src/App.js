b2c5fcf70100df34344be933f7485d890d2b9b1d
mindeleven <mindeleven@posteo.de>
2022-01-24 10:20:06 +0100

    Adding the username to the UI

--- src/App.js
+++ src/App.js
@@ -14,7 +14,10 @@ function App() {
       </div>
       <div className="displayUsers">
         {userList.map((user) => {
-          return <h1>{user.name}</h1>
+          return <div>
+            <h1>{user.name}</h1>
+            <h1>{user.username}</h1>
+          </div>
         })}
       </div>
     </div>
